e593faf893781b7b2e83ec20b6aa67b597bb3f7f
Eyoab Tesfaye <eyoabtesfaye10@gmail.com>
2021-02-16 11:41:52 +0300

    added password hashing and registration

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,20 @@ const express = require('express')
 const User = require('../models/User')
 
 // Register a user
-module.exports.signupUser = ((req, res) => {
-    res.send('passed')
-})
+module.exports.signupUser = async (req, res) => {
+    try {
+        const user = new User({...req.body})
+        let error = await user.validate()
+        if(error) throw(error)
+        await user.save()
+        // Todo: send a json web token to the user
+        res.send('User saved')
+    } catch (err) {
+        // email uniqueness error message
+        if (err.name === 'MongoError' && err.code === 11000) {
+            err.msg = 'User already exists'
+            return res.status(400).json({msg: err.msg})
+        }
+        res.status(500).json({msg: err.msg})
+    }
+}
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 // User schema
 const UserSchema = new mongoose.Schema({
@@ -20,6 +21,41 @@ const UserSchema = new mongoose.Schema({
         default: Date.now
     }
 })
+
+// ensure uniqueness of email fields
+UserSchema.index({ email: 1 }, { unique: true})
+
+// password hashing middleware before saving new records
+// Don't use an arrow function for the callback, as it changes the scope of 'this'
+UserSchema.pre('save', async function(next) {
+    let user = this
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next()
+    try {
+        hashedPwd = await user.hashPassword(user.password)
+        if(hashedPwd !== null) {
+            user.password = hashedPwd
+            next()
+        }
+        else throw({msg: 'Unable to hash password'})
+    } catch (err) {
+        return next(err)
+    }
+})
+
+// hash password
+UserSchema.methods.hashPassword = async (password) => {
+    try {
+        // generate a salt with rounds = 10
+        const salt = await bcrypt.genSalt(10)
+        // hash the password using our new salt
+        const hash = await bcrypt.hash(password, salt)
+        return hash
+    } catch (err) {
+        console.log(err.message)
+        return null
+    }
+}
 
 // User model
 module.exports = User = mongoose.model('User', UserSchema)
