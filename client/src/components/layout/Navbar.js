b3d52f7d620492f466f7c0b4dc21d08fe2a5a3e4
Eyoab Tesfaye <eyoabtesfaye10@gmail.com>
2021-02-18 11:58:40 +0300

    added navbar and router setup

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
+
+const Navbar = ({title, icon}) => (
+    <nav className="navbar navbar-expand-md bg-primary">
+        <h1>
+            <Link className="navbar-brand" to="/">
+                <i className={icon}/> {title}
+            </Link>
+        </h1>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <div className="dropdown-divider"></div>
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                    <Link to="/" className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/about" className="nav-link">About</Link>
+                </li>
+            </ul>
+        </div>
+    </nav>
+)
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired, 
+    icon: PropTypes.string
+}
+
+Navbar.defaultProps = {
+    title: 'Contact Keeper', 
+    icon: 'fa fa-id-card'
+}
+
+export default Navbar
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,9 @@
+const About = () => (
+    <div>
+        <h1>About This App</h1>
+        <p className="my-1">This is a full stack MERN app for keeping contacts</p>
+        <p className="bg-dark p"><strong>Version: </strong>1.0.0</p>
+    </div>
+)
+
+export default About
