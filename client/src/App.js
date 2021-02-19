d9476ad6b79b3998a620b2d80870ff68178908bc
Eyoab Tesfaye <eyoabtesfaye10@gmail.com>
2021-02-19 10:59:42 +0300

    setup auth context

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,26 @@ import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
 import ContactContextProvider from './context/contact/ContactContext'
+import AuthContextProvider from './context/auth/AuthContext'
 import './App.css'
 
 const App = () => {
   return (
-    <ContactContextProvider>
-      <Router>
-        <Fragment>
-          <Navbar/>
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home}></Route>
-              <Route exact path="/about" component={About}></Route>
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactContextProvider>
+    <AuthContextProvider>
+      <ContactContextProvider>
+        <Router>
+          <Fragment>
+            <Navbar/>
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home}></Route>
+                <Route exact path="/about" component={About}></Route>
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactContextProvider>
+    </AuthContextProvider>
   )
 }
 
---
+++ client/src/context/auth/AuthContext.js
@@ -0,0 +1,55 @@
+import {createContext, useState} from 'react'
+
+export const AuthContext = createContext()
+
+const AuthContextProvider = (props) => {
+    const [token, setToken] = useState(localStorage.getItem('token'))
+    const [isAuthenticated, setIsAuthenticated] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [user, setUser] = useState(null)
+    const [error, setError] = useState(null)
+
+    // load user
+    const loadUser = () => {
+
+    }
+
+    // register user
+    const registerUser = () => {
+
+    }
+
+    // login user
+    const loginUser = () => {
+
+    }
+
+    // logout
+    const logoutUser = () => {
+
+    }
+
+    // clear errors 
+    const clearErrors = () => {
+
+    }
+
+    return(
+        <AuthContext.Provider value={{
+            token, 
+            isAuthenticated, 
+            loading, 
+            user,
+            error,
+            loadUser, 
+            registerUser, 
+            loginUser,
+            logoutUser, 
+            clearErrors
+        }}>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContextProvider
